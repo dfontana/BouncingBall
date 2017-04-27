eb3cff9fa9a4b2d68dc6a3173779cfb847a4efec
Dylan Fontana <dyfontana@gmail.com>
2017-04-26 22:05:46 -0400

    Collision detection works. Can still leave level.

--- lib/ball.rb
+++ lib/ball.rb
@@ -10,7 +10,7 @@ class Ball
         @y = rand(@radius..(win.height - @radius) * 0.5)
         @vx = 2
         @vy = 3
-        @maxSpeed = 30
+        @maxSpeed = 15
         @image = Gosu::Image.new(win, 'media/ball.png', true)
     end
 
@@ -35,50 +35,102 @@ class Ball
         @vy += @gravity if @vy.abs < @maxSpeed
 
         # 3. Compute where the ball *would* land, if updated right now.
-        dX = @x + @vx
-        dY = @y + @vy
+        dx = @x + @vx
+        dy = @y + @vy
 
         # 4. Perform the collision detection.
         #       1) Look for collision with level walls
-        #           a) Check Left & Right; true? update dx,dy to collision point -> invert Vx -> break to update/return
-        #           b) Check Top & Bottom; true? update dx,dy to collision point -> invert Vy -> break to update/return
+        #           a) Check Top & Bottom; true? update dx,dy to collision point -> invert Vy -> break to update/return
+        #           b) Check Left & Right; true? update dx,dy to collision point -> invert Vx -> break to update/return
         #       2) Look for first instance of collision in each platform:
-        #           a) Check Left & Right; true? update dX,dY -> invert Vx -> break to update/return
-        #           b) Check Top & Bottom; true? update dX,dY -> invery Vy -> break to update/return
-        #       3) Update based on dX and dY (no need to return).
-        platforms.each do |plat|
-            coor = plat.topCollision?(self, dX, dY)
-            if coor.is_a?(Array)
-                # Collided, set the new destination to collision
-                puts "Collision Top"
-                # Invert the correct velocity
-                break
-            end
-            coor = plat.bottomCollision?(self, dX, dY)
-            if coor.is_a?(Array)
-                # Collided, set the new destination to collision
-                puts "Collision bottom"
-                # Invert the correct velocity
-                break
-            end
-            coor = plat.leftCollision?(self, dX, dY)
-            if coor.is_a?(Array)
-                # Collided, set the new destination to collision
-                puts "Collision Left"
-                # Invert the correct velocity
-                break
-            end
-            coor = plat.rightCollision?(self, dX, dY)
-            if coor.is_a?(Array)
-                # Collided, set the new destination to collision
-                puts "Collision Right"
-                # Invert the correct velocity
-                break
-            end
+        #           a) Check Top & Bottom; true? update dx,dy -> invery Vy -> break to update/return
+        #           b) Check Left & Right; true? update dx,dy -> invert Vx -> break to update/return
+        #       3) Update based on dx and dy (no need to return).
+
+        #1a
+        coor = topBottomCollision(dx, dy)
+        if coor
+            dx = coor[0]
+            dy = coor[1]
+            @vy = -@vy
         end
 
-        @x = dX
-        @y = dY
+        #1b
+        if !coor    
+            coor = leftRightCollision(dx, dy)
+            if coor
+                dx = coor[0]
+                dy = coor[1]
+                @vx = -@vx
+            end
+        end
+        
+        if !coor
+            platforms.each do |plat|
+                #2a
+                coor = plat.verticalCollision?(self, dx, dy)
+                if coor.is_a?(Array)
+                    dx = coor[0]
+                    dy = coor[1]
+                    @vy = -@vy
+                    break
+                end
+
+                #2b
+                coor = plat.horizontalCollision?(self, dx, dy)
+                if coor.is_a?(Array)
+                    dx = coor[0]
+                    dy = coor[1]
+                    @vx = -@vx
+                    break
+                end
+            end
+        end
+        
+
+        #3
+        @x = dx
+        @y = dy
+    end
+
+    def topBottomCollision(dx, dy)
+        bx = dx - @x   #p4.x - p3.x
+        by = dy - @y   #p4.y - p3.y
+
+        #top
+        coor = intersection(0, 0, @win.width, 0, bx, by, dx, dy) 
+        return coor if coor.is_a?(Array)
+
+        #bottom
+        return intersection(0, @win.height, @win.width, 0, bx, by, dx, dy) 
+    end
+
+    def leftRightCollision(dx, dy)
+        bx = dx - @x   #p4.x - p3.x
+        by = dy - @y   #p4.y - p3.y
+
+        #Left
+        coor = intersection(0, 0, 0, @win.height, bx, by, dx, dy) 
+        return coor if coor.is_a?(Array)
+
+        #bottom
+        return intersection(@win.width, 0, 0, @win.height, bx, by, dx, dy) 
+    end
+
+    # returns false if no intersection, else the intersection point
+    def intersection(x, y, rx, ry, bx, by, dx, dy)
+      #Determinant Check (Parallel or collinear)
+      return false if (rx * by) - (bx * ry) == 0
+      
+      #Paramters:
+      # t = ((p1.y-p4.y) * bx) - (by * (p1.x-p4.x)) / ((rx*by) - (bx*ry))
+      # u = ((p1.y-p4.y) * rx) - (ry * (p1.x-p4.x)) / ((rx*by) - (bx*ry))
+      t = ((y - dy) * bx - by * (x - dx)) / ((rx * by) - (bx * ry))
+      u = ((y - dy) * rx - ry * (x - dx)) / ((rx * by) - (bx * ry))
+      
+      #Collison check
+      return [x+(t*rx), y+(t*ry)] if u.between?(0,1) && t.between?(0,1)
+      return false
     end
 
     def getX
